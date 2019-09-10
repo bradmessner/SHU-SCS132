0b30b29e6d91acc761d283525aac79496cd0821f
Brad D Messner <bmessner@setonhill.edu>
2019-09-09 23:48:25 -0400

    Homework 1 Submitted

---
+++ Homework 1/src/homework1.java
@@ -0,0 +1,24 @@
+// Project:		Solution for Homework 1
+// Author:		Brad D. Messner
+
+
+public class homework1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to SCS132 - Introduction to Application Development");
+		System.out.println("Instructor:    Brad D. Messner");
+		System.out.println("Language Used: Java");
+		System.out.println("Credits:       3");
+		
+		// Note the \n at the beginning adds a blank line before this note.
+		System.out.println("\nIf you ever need help, don't hesitate to ask.  We have plenty of opportunities:");
+		System.out.println("\t1.) Office Hours");
+		System.out.println("\t2.) Open Programming Lab");
+		System.out.println("\t3.) Tutor");
+		System.out.println("\t4.) By Request");
+
+	}
+
+}
